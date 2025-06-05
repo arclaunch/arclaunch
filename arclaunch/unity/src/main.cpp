c01d2a0e8047fc4dae98dd3c134fea80e1b5f560
James Cahill <jcahill@ieee.org>
2025-06-05 10:40:05 +0100

    (unity): Add copy_dlls macro, run existing example simulation and send results back to unity

--- arclaunch/unity/src/main.cpp
+++ arclaunch/unity/src/main.cpp
@@ -2,11 +2,17 @@
 
 #include <boost/signals2.hpp>
 
+#include "jolt/provider.hpp"
+
 typedef void (*cbChar)(const char *message);
+typedef void (*cbPos)(const float x, const float y, const float z);
 
 // Signal type
 
-boost::signals2::signal<void(const char *)> signal;
+server::jolt::Provider *joltProvider;
+
+boost::signals2::signal<void(const char *)>
+    signal;
 
 AC_API void RegisterCallback(cbChar callback)
 {
@@ -20,4 +26,18 @@ AC_API void RegisterCallback(cbChar callback)
 AC_API void TriggerCallback()
 {
     signal("Hello from C++!");
+};
+
+AC_API void SetupSim(cbPos callback)
+{
+
+    joltProvider = new server::jolt::Provider();
+    joltProvider->posSignal.connect([callback](const JPH::Vec3 vec)
+                                    { callback(vec.GetX(), vec.GetY(), vec.GetZ()); });
+};
+
+AC_API void RunSim()
+{
+
+    joltProvider->initial();
 };
