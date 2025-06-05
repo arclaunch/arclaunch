0ce6d8be440c6869fe819b65b3085f9358efcb52
James Cahill <jcahill@ieee.org>
2025-06-05 10:39:03 +0100

    (cmake): Only have libgcc, libstdc++ and boost statically linked (have jolt and quiche be shared dlls)

--- cmake/quiche.cmake
+++ cmake/quiche.cmake
@@ -2,13 +2,14 @@ add_subdirectory(lib/corrosion)
 corrosion_import_crate(
     MANIFEST_PATH lib/quiche/Cargo.toml
     FEATURES ffi
+    CRATE_TYPES cdylib 
 )
 
 add_dependencies(_cargo-build_quiche ssl crypto)
 
 add_library(quiche-api INTERFACE)
 target_include_directories(quiche-api INTERFACE ${CMAKE_CURRENT_LIST_DIR}/../lib/quiche/quiche/include)
-target_link_libraries(quiche-api INTERFACE quiche ssl) # our boringssl for ssl and crypto
+target_link_libraries(quiche-api INTERFACE quiche)
 
 if (WIN32)
     target_link_libraries(quiche-api INTERFACE crypt32)
