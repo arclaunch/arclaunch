0ad20efc80f58c67bd13b2c5473b3502b41ba0b3
James Cahill <jcahill@ieee.org>
2025-06-17 18:29:14 +0100

    (cmake/quiche): dont link everything against boringssl[crypto]

--- cmake/quiche.cmake
+++ cmake/quiche.cmake
@@ -5,10 +5,11 @@ corrosion_import_crate(
     CRATE_TYPES cdylib 
 )
 
-add_dependencies(_cargo-build_quiche ssl crypto)
-
 add_library(quiche-api INTERFACE)
 target_include_directories(quiche-api INTERFACE ${CMAKE_CURRENT_LIST_DIR}/../lib/quiche/quiche/include)
+
+corrosion_link_libraries(quiche ssl crypto)
+
 target_link_libraries(quiche-api INTERFACE quiche)
 
 if (WIN32)
